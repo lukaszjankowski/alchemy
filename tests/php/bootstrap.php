8fc36f399536d2bd9855b94c1375f214d7f76d17
Łukasz Jankowski <mail@lukaszjankowski.info>
2013-05-06 20:14:10 +0200

    require vendor/autoload.php

--- tests/php/bootstrap.php
+++ tests/php/bootstrap.php
@@ -3,9 +3,7 @@ require_once dirname(__FILE__) . '/../../configs/constants.php';
 require_once 'testConfig.php';
 require_once dirname(__FILE__) . '/../../../configsGenerated/config.php';
 
-require_once 'Zend/Loader/Autoloader.php';
 $loader = Zend_Loader_Autoloader::getInstance();
 $loader->registerNamespace('Alchemy');
 
-require_once 'Zend/Application.php';
 require_once 'ControllerTestCase.php';
